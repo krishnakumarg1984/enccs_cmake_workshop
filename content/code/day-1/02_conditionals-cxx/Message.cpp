30e30db3b46266b87dacd7095fcd275414de5d62
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-12-12 13:40:53 +0000

    option variable to set shared library

--- content/code/day-1/02_conditionals-cxx/Message.cpp
+++ content/code/day-1/02_conditionals-cxx/Message.cpp
@@ -3,9 +3,8 @@
 #include <iostream>
 #include <string>
 
-std::ostream &Message::printObject(std::ostream &os) {
-  os << "This is my very nice message: " << std::endl;
-  os << message_;
-
-  return os;
+std::ostream& Message::printObject(std::ostream& os) {
+    os << "This is my very nice message: " << '\n';
+    os << message_;
+    return os;
 }
