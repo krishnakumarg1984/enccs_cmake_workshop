f463c8ed0a4d3590e4c1823491b6c62f14d17137
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-12-13 16:19:23 +0000

    finished fetchcontent catch2

--- content/code/day-2/26_more-catch2/sum_integers.cpp
+++ content/code/day-2/26_more-catch2/sum_integers.cpp
@@ -3,9 +3,10 @@
 #include <vector>
 
 int sum_integers(const std::vector<int> integers) {
-  auto sum = 0;
-  for (auto i : integers) {
-    sum += i;
-  }
-  return sum;
+    auto sum = 0;
+    for ( auto i : integers ) {
+        sum += i;
+    }
+
+    return sum;
 }
