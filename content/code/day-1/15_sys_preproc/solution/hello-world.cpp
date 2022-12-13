ca53eb8b1887273ea81c4500e8b9375912a0e1aa
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-12-13 16:37:28 +0000

    finished enccs course

--- content/code/day-1/15_sys_preproc/solution/hello-world.cpp
+++ content/code/day-1/15_sys_preproc/solution/hello-world.cpp
@@ -4,17 +4,18 @@
 
 std::string say_hello() {
 #ifdef IS_WINDOWS
-  return std::string("Hello from Windows!");
+    return std::string("Hello from Windows!");
 #elif IS_LINUX
-  return std::string("Hello from Linux!");
+    return std::string("Hello from Linux!");
 #elif IS_MACOS
-  return std::string("Hello from macOS!");
+    return std::string("Hello from macOS!");
 #else
-  return std::string("Hello from an unknown system!");
+    return std::string("Hello from an unknown system!");
 #endif
 }
 
 int main() {
-  std::cout << say_hello() << std::endl;
-  return EXIT_SUCCESS;
+    std::cout << say_hello() << '\n';
+
+    return EXIT_SUCCESS;
 }
